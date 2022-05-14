cc23ac8973bef53cff5d7484a07b9237a47f6d67
christopherginn <hey.im.chris@live.com>
2022-05-14 14:51:48 -0400

    POST sends body and is written to db.json

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,25 @@
 const path = require("path");
 const router = require("express").Router();
-const store = require ('../db/functions')
+const store = require ('../db/functions');
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    writeToFile,
+    readAndAppend,
+} = require('../helpers/fsUtils');
 
 router.get('/notes', (req, res) => {
     res.json('get!');
 });
 
 router.post("/notes", (req, res) => {
-    res.json('posted!');
-    store.addNote(req.body);
+    // res.json('posted!');
+    // store.addNote(req.body);
+    const newNote = req.body;
+    newNote.id = uuidv4(),
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(newNote);
 });
 
 

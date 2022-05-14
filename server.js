81d83e2c4d10cee4cd368a7ae920127fc493d4cd
christopherginn <hey.im.chris@live.com>
2022-05-14 13:49:24 -0400

    Routes were in the wrong order on html.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const html = require("./routes/html");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,12 +11,18 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.post("/api/notes", (req, res) => {
+    res.json('posted!')
 })
+
+app.use('/', html);
+
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
+
+// app.get("/notes", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/public/notes.html"));
+// })
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
